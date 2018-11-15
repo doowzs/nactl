e06b17cc6d7fd0715987c029a2eec994db6064b2
Tianyun Zhang <doowzs@qq.com>
2018-11-15 17:50:00 +0800

    [MOD] template.cpp
    move macros above 'using namespace std'.

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
-using namespace std;
-
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define trav(a, x) for(auto& a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
+using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
-	cin.sync_with_stdio(0); cin.tie(0);
-	cin.exceptions(cin.failbit);
-	int T = 0;
-	cin >> T;
-	while (T--) {
-		
-	}
-	return 0;
+  cin.sync_with_stdio(0); cin.tie(0);
+  cin.exceptions(cin.failbit);
+  int T = 0;
+  cin >> T;
+  while (T--) {
+
+  }
+  return 0;
 }
